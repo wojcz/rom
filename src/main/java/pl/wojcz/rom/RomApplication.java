810196f9c44137d11a4461a228aada7abbff747a
wojciech czarnecki <wojtek-cz@o2.pl>
2023-11-20 14:28:45 +0100

    project init

---
+++ src/main/java/pl/wojcz/rom/RomApplication.java
@@ -0,0 +1,13 @@
+package pl.wojcz.rom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wojcz/rom/RomApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojcz.rom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
