d45159846f41988c025aaf536fbf34faccaed7e9
wojciech czarnecki <wojtek-cz@o2.pl>
2023-11-22 18:47:41 +0100

    working solution after a good sleep

--- src/test/java/pl/wojcz/rom/OccupancyCheckTest.java
+++ src/test/java/pl/wojcz/rom/OccupancyCheckTest.java
@@ -2,116 +2,94 @@ package pl.wojcz.rom;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class OccupancyCheckTest {
 
     @Test
     public void mockTest() {
         //input
-        double[] potentialGuests = {23, 45, 155, 374, 22, 99.99, 100, 101, 115, 209};
-        //double[] potentialGuests = {154, 123, 103, 100, 100, 100, 182};
-        //double[] potentialGuests = {23, 56, 99, 83, 24, 56, 83};
-        //double[] potentialGuests = {23, 56, 99, 83, 24, 56, 83, 100, 100, 100, 99};
-        //int freePremium = 3, freeEconomy = 3;
-        //int freePremium = 7, freeEconomy = 5;
-        //int freePremium = 2, freeEconomy = 7;
-        int freePremium = 7, freeEconomy = 1;
+        double[] input = {23, 45, 155, 374, 22, 99.99, 100, 101, 115, 209};
+        //double[] input = {154, 123, 103, 100, 100, 100, 182};
+        //double[] input = {23, 56, 99, 83, 24, 56, 83};
+        //double[] input = {23, 56, 99, 83, 24, 56, 83, 100, 100, 100, 99};
+        //double[] input = {100, 200, 300, 100, 200, 300, 100, 200, 300};
+        //double[] input = {99.95, 30, 50, 23, 86, 34, 95, 23, 18, 17};
+        //int availablePremium = 3, availableEconomy = 3;
+        int availablePremium = 7, availableEconomy = 5;
+        //int availablePremium = 2, availableEconomy = 7;
+        //int availablePremium = 7, availableEconomy = 1;
+        //int availablePremium = 1, availableEconomy = 1;
+        //int availablePremium = 0, availableEconomy = 1;
         double priceLimit = 100;
 
-        System.out.println(Arrays.toString(potentialGuests));
-        System.out.println("Premium{"+freePremium+"} Economy{"+freeEconomy+"}");
+        //print output before calculation
+        System.out.println(Arrays.toString(input));
+        System.out.println("Premium: "+availablePremium+", Economy: "+availableEconomy);
 
         //prepare data
-        Arrays.sort(potentialGuests);
+        Arrays.sort(input);
+        Stack<Double> potentialGuests = new Stack<>();
+        for(double p : input)
+            potentialGuests.push(p);
+
+        //init output objects
         Usage usagePremium = new Usage("Premium");
         Usage usageEconomy = new Usage("Economy");
 
-        int splitIndex = -1;
-        for(int i=potentialGuests.length-1; i>=0; i--) {
-            if(potentialGuests[i] < priceLimit) {
-                splitIndex = i;
-                break;
+        //calculate
+        while(!potentialGuests.isEmpty()) {
+            double price = potentialGuests.pop();
+
+            //got available premium rooms for this price
+            if(availablePremium > 0 && price >= priceLimit) {
+                usagePremium.push(price);
+                --availablePremium;
+                continue;
+            }
+
+            //got available economy rooms for this price
+            if(availableEconomy > 0 && price < priceLimit) {
+                usageEconomy.push(price);
+                --availableEconomy;
+                continue;
+            }
+
+            //if we got available premium room for economy guests -> upgrade
+            if(availableEconomy == 0 && availablePremium > 0 && !usageEconomy.isEmpty()) {
+                usagePremium.push(usageEconomy.get(0)); //not using pop in order to get the highest price for upgrade
+                usageEconomy.remove(0);
+                usageEconomy.push(price);
+                --availablePremium;
             }
         }
-
-        double[] economyClients = {};
-        double[] premiumClients = potentialGuests;
-        if(splitIndex >= 0) {
-            economyClients = Arrays.copyOfRange(potentialGuests, 0, splitIndex+1);
-            premiumClients = Arrays.copyOfRange(potentialGuests, splitIndex+1, potentialGuests.length);
-        }
-
-        //calculate premium
-        for(int i=premiumClients.length-1; i>=0; i--) {
-            if(freePremium <= 0)
-                break;
-
-            usagePremium.add(premiumClients[i]);
-            --freePremium;
-        }
-
-        //calculate economy
-        int freeEconomyFinal = freeEconomy;
-        int clientsLeft;
-        for(clientsLeft=economyClients.length-1; clientsLeft>=0; clientsLeft--) {
-            if(freeEconomy <= 0)
-                break;
-
-            usageEconomy.add(economyClients[clientsLeft]);
-            --freeEconomy;
-        }
-        if(freePremium > 0) {
-            for (int i = clientsLeft+1; i >= 0; i--) {
-                if(freePremium <= 0)
-                    break;
-
-                usageEconomy.remove(economyClients[i]);
-                usagePremium.add(economyClients[i]);
-                --freePremium;
-            }
-        }
-
-
-//        for(int i=economyClients.length-1; i>=0; i--) {
-//            if(freePremium > 0 && economyClients.length>freeEconomyFinal) {
-//                usagePremium.add(economyClients[i]);
-//                --freePremium;
-//                continue;
-//            }
-//        }
 
         //output
         System.out.println(usagePremium);
         System.out.println(usageEconomy);
     }
 
-    public static class Usage {
+    public static class Usage extends Stack<Double> {
+
         private final String type;
-        private int quantity;
-        private double total;
 
         public Usage(String type) {
+            super();
             this.type = type;
         }
 
-        public void add(double price) {
-            ++quantity;
-            total+=price;
-        }
-
-        public void remove(double price) {
-            --quantity;
-            total-=price;
+        public Double getTotal() {
+            return stream().mapToDouble(Double::doubleValue).sum();
         }
 
         @Override
         public String toString() {
             return type+"{" +
-                    "quantity=" + quantity +
-                    ", total=" + total +
+                    "quantity=" + size() +
+                    ", total=" + getTotal() +
                     '}';
         }
+
     }
 
 }
